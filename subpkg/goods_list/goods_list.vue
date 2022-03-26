f3e79cac258955671b8389b10f2b0172fddfa9a1
jwf <2545538936@qq.com>
2022-03-26 16:29:38 +0800

    完成了home首页的开发

---
+++ subpkg/goods_list/goods_list.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    你好啊
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
