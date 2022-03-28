bb30aad09f289f665c5e807bfc4de697305f833b
jwf <2545538936@qq.com>
2022-03-28 21:16:16 +0800

    完成了商品详情页面

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,36 @@
 <template>
-  <view>
-    商品详情
+  <view v-if="goods_info.goods_name">
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item, i) in goods_info.pics" :key="i">
+        <image :src="item.pics_big" @click="preview(i)"></image>
+      </swiper-item>
+    </swiper>
+    <!-- 商品信息区域 -->
+    <view class="goods-info-box">
+      <!-- 商品价格 -->
+      <view class="price">￥{{goods_info.goods_price}}</view>
+      <!-- 信息主体区域 -->
+      <view class="goods-info-body">
+        <!-- 商品名称 -->
+        <view class="goods-name">{{goods_info.goods_name}}</view>
+        <!-- 收藏 -->
+        <view class="favi">
+          <uni-icons type="star" size="18" color="gray"></uni-icons>
+          <text>收藏</text>
+        </view>
+      </view>
+      <!-- 运费 -->
+      <view class="yf">快递：免运费</view>
+    </view>
+    <!-- 商品详情信息 -->
+    <rich-text :nodes="goods_info.goods_introduce"></rich-text>
+    <!-- 底部区域-->
+    <view class="goods-carts">
+      <uni-goods-nav :options="options" :fill="true" :button-group="buttonGroup" @click="onClick"
+        @buttonClick="buttonClick" />
+    </view>
+
   </view>
 </template>
 
@@ -8,12 +38,132 @@
   export default {
     data() {
       return {
-        
+        // 商品详情对象
+        goods_info: {},
+        options: [{
+          icon: 'shop',
+          text: '店铺',
+          info: 0,
+          infoBackgroundColor: '#007aff',
+          infoColor: "red"
+        }, {
+          icon: 'cart',
+          text: '购物车',
+          info: 2
+        }],
+        buttonGroup: [{
+            text: '加入购物车',
+            backgroundColor: '#ff0000',
+            color: '#fff'
+          },
+          {
+            text: '立即购买',
+            backgroundColor: '#ffa200',
+            color: '#fff'
+          }
+        ]
       };
+    },
+    //生命周期函数
+    onLoad(options) {
+      // 获取商品 Id
+      const goods_id = options.goods_id
+      // 调用请求商品详情数据的方法
+      this.getGoodsDetail(goods_id)
+    },
+    methods: {
+      // 定义请求商品详情数据的方法
+      async getGoodsDetail(goods_id) {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/detail', {
+          goods_id
+        })
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 使用字符串的 replace() 方法，将 webp 的后缀名替换为 jpg 的后缀名
+        res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;" ')
+          .replace(/webp/g, 'jpg')
+        this.goods_info = res.message
+      },
+      //轮播图的预览效果
+      preview(i) {
+        uni.previewImage({
+          current: i,
+          urls: this.goods_info.pics.map(x => x.pics_big)
+        })
+      },
+      //底部区域点击函数
+      onClick(e) {
+        if(e.content.text==='购物车'){
+          uni.switchTab({
+            url:'../../pages/cart/cart'
+          })
+        }
+      },
+      buttonClick(e) {
+        console.log(e)
+        this.options[2].info++
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  swiper {
+    height: 750rpx;
 
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  // 商品信息区域的样式
+  .goods-info-box {
+    padding: 10px;
+    padding-right: 0;
+
+    .price {
+      color: #c00000;
+      font-size: 18px;
+      margin: 10px 0;
+    }
+
+    .goods-info-body {
+      display: flex;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+        padding-right: 10px;
+      }
+
+      // 收藏区域
+      .favi {
+        width: 120px;
+        font-size: 12px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border-left: 1px solid #efefef;
+        color: gray;
+      }
+    }
+
+    // 运费
+    .yf {
+      margin: 10px 0;
+      font-size: 12px;
+      color: gray;
+    }
+  }
+
+  .goods-carts {
+    // 为商品导航组件添加固定定位
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
 </style>
